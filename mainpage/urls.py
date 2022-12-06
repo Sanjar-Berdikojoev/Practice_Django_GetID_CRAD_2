b877e5d2176c749ffc3e65053f6f4b8f313bb807
Sanjar-Berdikojoev <sanjarberdikojoev@gmail.com>
2022-12-06 10:36:45 +0600

    Last Changes

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('posts/',views.mainpage_all,name='posts'),
-    path('posts/<int:id>/', views.detail, name='show_detail'),
+    path('post/',views.mainpage_all,name='post'),
+    path('post/<int:id>/', views.detail, name='show_detail'),
+    path('add-post/', views.PostCreate.as_view(), name='add-post'),
+    path('post/<int:id>/update/', views.PostUpdate.as_view(), name='show_detail'),
+    path('post/<int:id>/delete/', views.PostDelete.as_view(), name='show_detail'),
+    path('posts/', views.Post.as_view(), name='posts'),
+    path('posts-2/', views.PostDetail.as_view(), name='posts-2'),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from . import models
+from django.shortcuts import render, get_object_or_404
+from . import models, forms
+from django.views import generic
+from .models import PC_Details
 
 def mainpage_all(request):
     post = models.PC_Details.objects.all()
@@ -8,4 +10,49 @@ def mainpage_all(request):
 def detail(request, id):
     post = models.PC_Details.objects.get(id=id)
     return render(request, 'index2.html', {'post': post})
+
+class Post(generic.ListView):
+    template_name = 'Post.html'
+    queryset = models.PC_Details.objects.all()
+
+    def get_queryset(self):
+        return models.PC_Details.objects.all()
+
+class PostDetail(generic.DetailView):
+    template_name = 'PostDetail.html'
+
+    def get_object(self, **kwargs):
+        post_id = self.kwargs.get('id')
+        return get_object_or_404(models.PC_Details, id=post_id)
+
+class PostCreate(generic.CreateView):
+    template_name = 'PostCreate.html'
+    form_class = forms.PostForm
+    queryset = models.PC_Details.objects.all()
+    success_url = '/post/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(PostCreate, self).form_valid(form=form)
+
+class PostUpdate(generic.UpdateView):
+    template_name = 'PostUpdate.html'
+    form_class = forms.PostForm
+    success_url = '/post/'
+
+    def get_object(self, **kwargs):
+        post_id = self.kwargs.get('id')
+        return get_object_or_404(models.PC_Details, id=post_id)
+
+    def form_valid(self, form):
+        return super(PostUpdate, self).form_valid(form=form)
+
+class PostDelete(generic.DeleteView):
+    template_name = 'PostDelete.html'
+    success_url = '/post/'
+
+    def get_object(self, **kwargs):
+        post_id = self.kwargs.get('id')
+        return get_object_or_404(models.PC_Details, id=post_id)
+
 # Create your views here.
